04cee742bf3e8ff659d39aabad72ce1fd6abd95b
Joan Ginard <joan.ginard@wizeline.com>
2024-01-31 12:46:38 +0100

    calling openai from the server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,44 @@
 "use server";
 
-export const generateRandomStringFromServer = async () => {
-  return Math.random().toString(36).substring(7);
+import { PromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { JsonOutputFunctionsParser } from "langchain/output_parsers";
+
+export const generateRandomStringFromServer: () => Promise<string> = async () => {
+  const prompt = PromptTemplate.fromTemplate(`Hello there! can you tell me a random unusual joke about {thing}?`);
+
+  const llm = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      temperature: 0.5,
+      // fail at the first rate limit error
+      maxRetries: 5,
+      // no more than two queries at the same time
+  });
+
+  const chain = prompt
+      .pipe(llm.bind({
+          functions: [{
+              name: "extractor",
+              description: "The given joke text",
+              parameters: {
+                  type: "object",
+                  properties: {
+                      html: {
+                          type: "string",
+                          description: "the text of the joke in simple html format",
+                      },
+                      text: {
+                          type: "string",
+                          description: "the text of the joke without formatting in simple text",
+                      },
+                  },
+                  required: ["html", "text"],
+              },
+          }],
+          function_call: { name: "extractor" },
+      }))
+      .pipe(new JsonOutputFunctionsParser());
+
+
+  return chain.invoke({ thing: "dogs" }).then(({ text }) => text);
 };
